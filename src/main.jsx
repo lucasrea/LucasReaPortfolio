aed2d7f7ab4c0cffb8ba28280d7ee9e5c225cc20
lucasrea <lucas.rea1998@gmail.com>
2024-03-23 18:51:13 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <h1>LUCAS REA Page</h1>
     <h2>HELLO WORLD</h2>
+    <h3>another code change</h3>
   </div>
   // {/* </React.StrictMode> */}
   // ,
