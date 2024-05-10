8fd85d646ed34df72c07bc327a8b480f9f4ae935
lucasrea <lucas.rea1998@gmail.com>
2024-05-09 23:36:22 -0400

    updates

--- src/components/AboutMeSmall.jsx
+++ src/components/AboutMeSmall.jsx
@@ -1,19 +1,19 @@
-import {motion} from "framer-motion";
-import {forwardRef} from "react";
+import { motion } from "framer-motion";
+import { forwardRef } from "react";
 
 import bnwPic from "../assets/bnwPic.jpeg";
 import pic from "../assets/pic.png";
 
-import {slideIn, textVariant} from "../constants/motion";
+import { slideIn, textVariant } from "../constants/motion";
 import ScrollButton from "./ScrollButton";
 
-const AboutMeSmall = forwardRef(({aboutMeRef}, ref) => {
+const AboutMeSmall = forwardRef(({ aboutMeRef }, ref) => {
   return (
-        <motion.div
-          variants={slideIn("down", "", 0, 1)}
-          initial={"hidden"}
-          whileInView={"show"}
-          className="z-10
+    <motion.div
+      variants={slideIn("down", "", 0, 1)}
+      initial={"hidden"}
+      whileInView={"show"}
+      className="z-10
           bg-opacity-30
           bg-white
           relative 
@@ -32,21 +32,28 @@ const AboutMeSmall = forwardRef(({aboutMeRef}, ref) => {
           justify-center 
           items-center 
           md:drop-shadow-lg"
-          style={{ marginRight: '50px', marginTop: `10px` }}
-        >
-          <div className="flex justify-center items-end md:items-center md:justify-start p-1">
-            <img src={pic} className="block dark:hidden md:w-48 md:h-64 w-40 h-10 rounded-xl" />
-            <img src={pic} className="hidden dark:block md:w-48 md:h-64 w-40 h-10 rounded-xl" />
-          </div>
-          <div className="p-5 md:w-full">
-            <div className="text-lg leading-9  text-white">
-              <span>
-                 Welcome to my page!
-              </span>
-            </div>
-          </div>
-        </motion.div>
-
+      style={{ marginRight: "50px", marginTop: `10px` }}
+    >
+      <div className="flex justify-center items-end md:items-center md:justify-start p-1">
+        <img
+          src={pic}
+          className="block dark:hidden md:w-48 md:h-64 w-40 h-10 rounded-xl"
+        />
+        <img
+          src={pic}
+          className="hidden dark:block md:w-48 md:h-64 w-40 h-10 rounded-xl"
+        />
+      </div>
+      <div className="p-5 md:w-full">
+        <div className="text-lg leading-9  text-white">
+          <span>
+            Hey there, welcome to my portfolio website! Below, I have a timeline
+            of my Software Engineering journey. On this site, I also highlight
+            my experience, skills, and projects - enjoy!{" "}
+          </span>
+        </div>
+      </div>
+    </motion.div>
   );
 });
 
